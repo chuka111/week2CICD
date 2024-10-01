e2e812261bfdee4ddd628be97ebb8b58002df7f1
chuka orajuba <g00414637@atu.ie>
2024-10-01 10:36:45 +0100

    new project

---
+++ src/test/java/ie/atu/week2springboot/Week2springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2springbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
