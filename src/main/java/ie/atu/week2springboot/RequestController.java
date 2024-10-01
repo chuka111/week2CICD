67c990617cfa228247bb4eb02a88f700ffe17888
chuka orajuba <g00414637@atu.ie>
2024-10-01 11:56:25 +0100

    created a calculator method

--- src/main/java/ie/atu/week2springboot/RequestController.java
+++ src/main/java/ie/atu/week2springboot/RequestController.java
@@ -18,5 +18,25 @@ public class RequestController {
         return "name " + name + " age " + age;
     }
 
+    @GetMapping("/calculator")
+    public String getCalculation(@RequestParam String operation, @RequestParam float num1, @RequestParam float num2){
+        switch(operation) {
+            case "add":
+                return "Operation "+ operation + " Answer " + (num1+num2);
+            case "subtract":
+                return "Operation "+ operation + " Answer " + (num1-num2);
+            case "multiply":
+                return "Operation "+ operation + " Answer " + (num1*num2);
+            case "divide":
+                if(num2 == 0){
+                    throw new IllegalArgumentException("cannot divide by zero");
+                }
+                else{
+                    return "Operation "+ operation + " Answer " + (num1/num2);
+                }
 
+            default:
+                throw new IllegalArgumentException("invalid");
+        }
+    }
 }
