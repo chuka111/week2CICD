d094b01ad9f2bd29d0c5106753dc724276160f4d
chuka orajuba <g00414637@atu.ie>
2024-10-01 11:15:35 +0100

    using requestparam command

--- src/main/java/ie/atu/week2springboot/RequestController.java
+++ src/main/java/ie/atu/week2springboot/RequestController.java
@@ -1,9 +1,6 @@
 package ie.atu.week2springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
@@ -16,7 +13,10 @@ public class RequestController {
     public String getGreeting(@PathVariable String name) {
         return "greeting " + name;}
 
-
+    @GetMapping("/details")
+    public String getDetails(@RequestParam String name, @RequestParam int age){
+        return "name " + name + " age " + age;
+    }
 
 
 }
